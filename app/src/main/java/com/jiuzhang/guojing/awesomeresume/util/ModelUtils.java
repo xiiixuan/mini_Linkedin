9c63285bb6deec48c0fc9b0684fa1e40c4b1cd50
guojing <guojing@jiuzhang.com>
2016-07-13 21:17:30 -0700

    Removed Toolbar

--- app/src/main/java/com/jiuzhang/guojing/awesomeresume/util/ModelUtils.java
+++ app/src/main/java/com/jiuzhang/guojing/awesomeresume/util/ModelUtils.java
@@ -7,9 +7,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
-import java.util.List;
-
 public class ModelUtils {
 
     private static Gson gson = new Gson();
